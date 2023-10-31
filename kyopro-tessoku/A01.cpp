bd615442803cc237b9afa3f633a8eb9b4ea38333
Satorien <130881456+Satorien@users.noreply.github.com>
2023-10-31 23:13:54 +0900

    Create A01.cpp

---
+++ kyopro-tessoku/A01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+//C++では上のようにライブラリを追加する
+
+int main(){
+  int n;
+  cin >> n; //標準入力の受け取り
+  cout << n*n <<endl; //標準出力
+  return 0; //C++ではmain関数は必ずint型でなければならない
+}
